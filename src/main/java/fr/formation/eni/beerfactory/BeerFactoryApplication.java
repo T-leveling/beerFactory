a1d6e3d19f90fac21e8c0f51ce41e9a9c18ae797
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-01-31 10:29:02 +0100

    Initial commit

---
+++ src/main/java/fr/formation/eni/beerfactory/BeerFactoryApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.eni.beerfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerFactoryApplication.class, args);
+    }
+
+}
